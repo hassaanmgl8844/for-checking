b118e4db0d66beb135950316e7902ccd2f68371c
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-27 19:56:53 +0500

    Tablet Version Finished

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -14,15 +14,22 @@ export const HeroSection = () => {
           />
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
             <div className="bg-green-500 size-2.5 rounded-full"></div>
-            <div className="text-sm font-medium">Available For New Projects</div>
+            <div className="text-sm font-medium">
+              Available For New Projects
+            </div>
           </div>
         </div>
-        <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">Building Exceptional User Experiences.</h1>
-        <p className="mt-4 text-center text-white/60">
-          I specialize in transforming designs into functional, high-performing
-          web applications. Let&apos;s discuss your next project.
-        </p>
-        <div className="flex flex-col items-center mt-8 gap-4">
+        <div className="max-w-lg mx-auto">
+          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            Building Exceptional User Experiences.
+          </h1>
+          <p className="mt-4 text-center text-white/60 md:text-lg">
+            I specialize in transforming designs into functional,
+            high-performing web applications. Let&apos;s discuss your next
+            project.
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
           <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
             <span className="font-semibold">Explore My Work</span>
             <ArrowDown className="size-4" />
