2251e833bc8ea25e162d24d8310cb30bb9f4a154
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-27 19:26:12 +0500

    For Checking Purpose

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Inter, Calistoga } from "next/font/google";
+import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: "400",
+});
+
+export const metadata: Metadata = {
+  title: "Hassaam's Portfolio",
+  description:
+    "I’m Hassaam, a freelance web developer and designer specializing in creating visually appealing and functional websites. Let's bring your ideas to life together!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={twMerge(
+          inter.variable,
+          calistoga.variable,
+          "bg-gray-900 text-white antialiased font-sans"
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
