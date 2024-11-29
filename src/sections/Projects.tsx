763baf6ff11635613ae8ddc6563f0b810d390b19
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-29 18:10:38 +0500

    Projects Section Mobile Responsiveness Done

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -2,6 +2,9 @@ import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import Image from "next/image";
+import CheckCircleIcon from "@/assets/icons/check-circle.svg";
+import ArrowUpRight from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg"
 
 const portfolioProjects = [
   {
@@ -44,7 +47,7 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
   return (
-    <div>
+    <section className="pb-16">
       <div className="container">
         <div className="flex justify-center">
           <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400  text-center bg-clip-text text-transparent">
@@ -57,38 +60,49 @@ export const ProjectsSection = () => {
         <p className="text-center text-white/60 mt-4">
           See How I Transformed concepts into engaging digital experiences.
         </p>
-        <div className="flex flex-col mt-10">
+        <div className="mt-10 flex flex-col gap-20">
           {portfolioProjects.map((project) => (
             <div
               key={project.title}
-              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20"
+              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8"
             >
-              <div className="flex">
-                <div className="bg-gradient-to-r from-emerald-300 to-sky-400">
+              <div className="absolute inset-0 -z-10 opacity-5" style={{
+                backgroundImage: `url(${grainImage.src})`
+              }}></div>
+                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2  font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
                   <span> {project.company}</span>
+                  <span>&bull;</span>
                   <span>{project.year}</span>
                 </div>
-              </div>
-              <h3>{project.title}</h3>
-              <hr />
+              <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
+              <hr className="border-t-2 border-white/5 mt-4" />
               {/* <ul>
                 {project.results.map((result) => (
                   <li>{result.title}</li>
                 ))}
               </ul> */}
-              <ul>
+              <ul className="flex flex-col gap-4 mt-4">
                 {project.results.map((result) => (
-                  <li key={result.title}>{result.title}</li>
+                  <li
+                    key={result.title}
+                    className="flex gap-2 text-sm text-white/50"
+                  >
+                    <CheckCircleIcon className="size-5" />
+                    <span className="">{result.title}</span>
+                  </li>
                 ))}
               </ul>
               <a href={project.link}>
-                <button>View Live Site</button>
+                <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                  <span>Visit Live Site </span>
+                  <ArrowUpRight className="size-4" />
+                </button>
               </a>
-              <Image src={project.image} alt={project.title} />
+              <Image src={project.image} alt={project.title} className="mt-8 -mb-4" />
             </div>
           ))}
         </div>
       </div>
-    </div>
+    </section>
   );
 };
