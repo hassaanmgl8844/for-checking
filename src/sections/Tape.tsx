86afed6990d5c16be0928e9093e547694325943c
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-29 21:55:34 +0500

    Text Is Added Working on Styles of Tape Section

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,29 @@
+import StarIcon from "@/assets/icons/star.svg";
+
+const words = [
+  "Performant",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scaleable",
+  "User Friendly",
+  "Maintainable",
+  "Search Optimized",
+  "Usable",
+  "Reliable",
+];
+
 export const TapeSection = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="flex gap-4">
+        {words.map((word) => (
+          <div key={word} className="inline-flex gap-4">
+            <span>{word}</span>
+            <StarIcon className="size-6" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
