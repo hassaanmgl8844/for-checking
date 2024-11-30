cb015f2602369b7dc7c6ac309cedefd11fd4c8eb
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-30 23:18:02 +0500

    Working on the 4th Card of the About Component

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -74,6 +74,16 @@ export const AboutSection = () => {
             ))}
           </div>
         </Card>
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>Beyond The Code</h3>
+            <p>
+              Explore my interests and hobbies beyond the digital realm.
+            </p>
+          </div>
+        </Card>
+        <Card></Card>
       </div>
     </div>
   );
