66c7cf96f6d144df2a6afbd90b3968626400a625
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-30 22:24:32 +0500

    Some Mistakes Is Fixed

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -55,7 +55,7 @@ export const TestimonialsSection = () => {
             {testimonials.map((testimonial) => (
               <Card key={testimonial.name} className="max-w-xs">
                 <div className="flex gap-4 items-center">
-                  <div className="size-14 bg-gray-700 inline-flex rounded-full items-center justify-center">
+                  <div className="size-14 bg-gray-700 inline-flex rounded-full items-center justify-center flex-shrink-0">
                     <Image
                       src={testimonial.avatar}
                       alt={testimonial.name}
