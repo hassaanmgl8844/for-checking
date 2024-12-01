43badcff35c63efe89250bd2edb1fd12d80cf39c
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-12-02 03:47:31 +0500

    Projects Section with Animations is Now Complete

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -51,12 +51,19 @@ export const ProjectsSection = () => {
   return (
     <section className="pb-16 lg:py-24">
       <div className="container">
-        <SectionHeader eyebrow="Real-World Results" title="Featured Projects" description="See How I Transformed concepts into engaging digital experiences." />
+        <SectionHeader
+          eyebrow="Real-World Results"
+          title="Featured Projects"
+          description="See How I Transformed concepts into engaging digital experiences."
+        />
         <div className="mt-10 md:mt-20 flex flex-col gap-20">
-          {portfolioProjects.map((project) => (
+          {portfolioProjects.map((project, projectIndex) => (
             <Card
               key={project.title}
-              className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20"
+              className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky "
+              style={{
+                top: `calc(64px + ${projectIndex * 40}px) `,
+              }}
             >
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
@@ -69,11 +76,6 @@ export const ProjectsSection = () => {
                     {project.title}
                   </h3>
                   <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
-                  {/* <ul>
-                {project.results.map((result) => (
-                  <li>{result.title}</li>
-                ))}
-              </ul> */}
                   <ul className="flex flex-col gap-4 mt-4 md:mt-5">
                     {project.results.map((result) => (
                       <li
