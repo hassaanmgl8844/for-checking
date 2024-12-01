d7a694a756bd177ce08b6fb7f8ee4a7405c37af1
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-12-01 22:44:22 +0500

    First 2 Sections of Tablet view is Done of About Page

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -97,31 +97,32 @@ export const AboutSection = () => {
           description="Learn more About Who I am, What I do, and What Inspires Me."
         />
         <div className="mt-20 flex flex-col gap-8">
-          <Card className="h-[320px]">
-            <CardHeader
-              title="My Reads"
-              description="Explore the Books Shaping My Perspectives."
-            />
-            <div className="w-40 mx-auto mt-8">
-              <Image src={bookImage} alt="Book Cover" />
-            </div>
-          </Card>
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-5">
+            <Card className="h-[320px] md:col-span-2">
+              <CardHeader
+                title="My Reads"
+                description="Explore the Books Shaping My Perspectives."
+              />
+              <div className="w-40 mx-auto mt-2 md:mt-0">
+                <Image src={bookImage} alt="Book Cover" />
+              </div>
+            </Card>
 
-          <Card className="h-[320px] p-0">
-            <CardHeader
-              title="My Toolbox"
-              description="Explore the technologies and tools I use to craft exceptional
+            <Card className="h-[320px] md:col-span-3">
+              <CardHeader
+                title="My Toolbox"
+                description="Explore the technologies and tools I use to craft exceptional
                 digital experiences."
-              className="px-6 pt-6"
-            />
-            <ToolboxItems items={toolboxItems} className="mt-6" />
-            <ToolboxItems
-              items={toolboxItems}
-              className="mt-6 "
-              itemsWrapperClassName="-translate-x-1/2"
-            />
-          </Card>
-
+                className=""
+              />
+              <ToolboxItems items={toolboxItems} className="mt-6" />
+              <ToolboxItems
+                items={toolboxItems}
+                className="mt-6"
+                itemsWrapperClassName="-translate-x-1/2"
+              />
+            </Card>
+          </div>
           <Card className="h-[320px] p-0 flex flex-col">
             <CardHeader
               title="Beyond The Code"
