762cf34d4cc089a76dc98ad3a148ce10ecb40a88
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-12-02 04:13:16 +0500

    Tape Section Completed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { transform } from "next/dist/build/swc";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -25,17 +26,26 @@ const config: Config = {
         sans: "var(--font-sans)",
         serif: "var(--font-serif)",
       },
-      animation:{
-        'ping-large': "ping-large 1s ease-in-out infinite",
+      animation: {
+        "ping-large": "ping-large 1s ease-in-out infinite",
+        "move-left": "move-left 1s linear infinite",
       },
-      keyframes:{
-        'ping-large': {
-          '75%, 100%':{
-            transform: 'scale(3)',
-            opacity: '0',
-          }
-        }
-      }
+      keyframes: {
+        "ping-large": {
+          "75%, 100%": {
+            transform: "scale(3)",
+            opacity: "0",
+          },
+        },
+        "move-left": {
+          "0%": {
+            transform: "translateX(0)",
+          },
+          "100%": {
+            transform: "translateX(-50%)",
+          },
+        },
+      },
     },
   },
   plugins: [],
