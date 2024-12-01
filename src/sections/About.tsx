f6014e6131933c73c976f0eb02de6cd10bfa8255
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-12-01 22:23:24 +0500

    About Page Mobile Section Done

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -146,9 +146,15 @@ export const AboutSection = () => {
               ))}
             </div>
           </Card>
-          <Card>
-            <Image src={mapImage} alt="Map Image" />
-            <Image src={smileMemoji} alt="Smile Memoji" />
+          <Card className="h-[320px] p-0 relative">
+            <Image
+              src={mapImage}
+              alt="Map Image"
+              className="h-full w-full object-cover object-left-top"
+            />
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:rounded-full after:-outline-offset-2 after:outline-gray-950/30">
+              <Image src={smileMemoji} alt="Smile Memoji" className="size-20" />
+            </div>
           </Card>
         </div>
       </div>
